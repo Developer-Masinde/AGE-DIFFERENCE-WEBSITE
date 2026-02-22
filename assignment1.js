f6042ce456a3f4049edc7ae6365ce66d19e05860
Joel Masinde. <otingajoel@gmail.com>
2026-02-23 01:10:10 +0300

    commit

--- assignment1.js
+++ assignment1.js
@@ -1,11 +1,35 @@
+// Named function
+// Named Function
+function enterDetails( county,name, gender,age) {
+  return county +name + gender + age;
+}
 
 
-// 
+
+
+// Arrow Function
+const checkpasspo = (details) =>{
+  return details;
+  county + name + gender + age;
+};
+
+
 // Programme1
 age = 18;
 
 if (age < 18) document.write("Am a child <br>");
 if (age >= 18) document.write("Am an adult <br>");
+
+
+function runProgram() {
+  let county = document.getElementById("county").value;
+  let name = parseInt(document.getElementById("name").value);
+  let gender = parseInt(document.getElementById("gender").value);
+  let age = parseInt(document.getElementById("age").value);
+
+  let details = enterDetails(county,name,gender,age);
+ displayResult(county,name,gender,age);
+}
 
 // // Javascript Events
 
