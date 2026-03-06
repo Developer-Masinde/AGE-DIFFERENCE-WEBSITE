1aef2e93770465247d3cb483b7a10a3caa4d6db2
Joel Masinde. <otingajoel@gmail.com>
2026-03-06 17:59:41 +0300

    commit

--- calculator/coffee.js
+++ calculator/coffee.js
@@ -1,10 +1,14 @@
-// programme.
-const result = document.getElementById("result");
-
-function totalDisplay() {
-  result.value = eval(result.value);
+function total() {
+  var Cappuccino;
+  var Caffe latte;
+  var White Coffee;
+  var sum;
+   Cappuccino= parseInt(document.getElementById("qty1").value);
+   Caffe latte= parseInt(document.getElementById("qty2").value);
+   White Coffee= parseInt(document.getElementById("qty3").value);
+  sum = Cappuccino * 250 + Caffe latte  * 300 + White Coffee * 150;
+  document.getElementById("Total-cost").innerHTML = "Ksh " + sum + ".00";
 }
-
-
-
-
+function submit() {
+  alert("Your Order has been Submitted, Successfully!");
+}
